01ff40cde4876bb21a9319bc27d1c4475cb3bf3e
GyeneseD <gyenese94@gmail.com>
2026-05-07 16:21:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:8080/api/login", {
+      const response = await fetch("/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
